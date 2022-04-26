2960639b0c6d2069ab5f88ec06b89be556821e60
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-26 17:04:43 -0300

    # commit - 2 - injeção de dependência

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #Injeção de Dependência
+### É uma forma de Inversão de Controle (IOC):
+````
+Injeta uma determinada dependência na instanciação de uma classe(vide Main).
+````
--- src/com/tuyo/injdep/Main.java
+++ src/com/tuyo/injdep/Main.java
@@ -2,6 +2,7 @@ package com.tuyo.injdep;
 
 import com.tuyo.injdep.modelo.Cliente;
 import com.tuyo.injdep.notificacao.Notificador;
+import com.tuyo.injdep.notificacao.NotificadorEmail;
 import com.tuyo.injdep.notificacao.NotificadorSMS;
 import com.tuyo.injdep.service.AtivacaoClienteService;
 
@@ -9,12 +10,15 @@ public class Main {
     public static void main(String[] args) {
 
         /*Instanciando os clientes:*/
-        Cliente joao = new Cliente("JoÃ£o", "joao@xyz.com", "3499998888");
+        Cliente joao = new Cliente("João", "joao@xyz.com", "3499998888");
         Cliente maria = new Cliente("Maria", "maria@xyz.com", "1177772222");
 
+        /*INJEÇÃO DE DEPENDÊNCIA: Intanciando o NotificadorSMS(ou NotificadorEmail)
+        injetando o notificador na instanciação de AtivacaoClienteService */
         Notificador notificador = new NotificadorSMS();
 
         /*Instanciando o serviço de ativação de cliente:*/
+        /*injetando o 'notificador' na instanciação de AtivacaoClienteService:*/
         AtivacaoClienteService ativacaoCliente = new AtivacaoClienteService(notificador);
 
         /*Ativando os dois clientes*/
